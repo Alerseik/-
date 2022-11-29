d6eafb9beb970015518d20aa252e6c8ed43d10ae
Alerseik <118421143+Alerseik@users.noreply.github.com>
2022-11-29 15:24:51 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using InspectorLib;
+
+namespace контрольная_работа
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            FunctionInsp funct = new FunctionInsp();
+            funct.GetInspector();// 1 задание
+            funct.GetСarInspection();// 2 задание
+            funct.SetInspector();// 3 задание
+
+
+            funct.GenerateNumber();// 4 задание
+            funct.GetWorker(); // 5 задание
+            funct.AddWorker(); // 6 задание
+            
+        }
+      
+
+
+
+
+
+    }
+}
